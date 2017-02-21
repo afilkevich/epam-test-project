f8db1ea0e38195246331a9c699e45b0942b32153
afilkevich <afilkevich@ya.ru>
2017-02-21 14:28:28 +0300

    with negative expecte

--- service/src/main/java/com/epam/test/UserServiceImpl.java
+++ service/src/main/java/com/epam/test/UserServiceImpl.java
@@ -1,7 +1,12 @@
 package com.epam.test;
 
 import com.epam.test.model.User;
+import com.epam.test.model.UserDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -9,33 +14,63 @@ import java.util.List;
  * Created by master on 21.2.17.
  */
 public class UserServiceImpl implements UserService {
+    private static final Logger LOGGER= LogManager.getLogger();
+
+    @Autowired
+    UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     @Override
     public List<User> getAllUsers() throws DataAccessException {
-        return null;
+        return userDao.getAllUsers();
     }
 
     @Override
     public User getUserById(Integer id) throws DataAccessException {
-        return null;
+        LOGGER.debug("getUserByID()",id);
+        Assert.notNull(id);
+
+        return userDao.getUserById(id);
     }
 
     @Override
     public User getUserByLogin(String login) throws DataAccessException {
-        return null;
+        LOGGER.debug("getUserByLogin()",login);
+        Assert.hasLength(login,"user login shouldnot be null");
+        return userDao.getUserbyLogin(login);
+
     }
 
     @Override
     public Integer addUser(User user) throws DataAccessException {
-        return null;
+        Assert.notNull(user,"user should not be null");
+        LOGGER.debug("addUser()",user);
+        Assert.isNull(user.getUserId(),"id should be null");
+        Assert.hasText(user.getLogin(),"login should not be null");
+        Assert.hasText(user.getPassword(),"password should not be null");
+        //Assert.isNull(userDao.getUserbyLogin(user.getLogin()),"login should be original");
+        return userDao.addUser(user);
+
     }
 
     @Override
     public int updateUser(User user) throws DataAccessException {
-        return 0;
+        Assert.notNull(user,"user not be null");
+        LOGGER.debug("updateUser",user);
+        Assert.hasText(user.getLogin(),"login should not be null");
+        Assert.hasText(user.getPassword(),"password should not be null");
+
+        return userDao.updateUser(user);
     }
 
     @Override
     public int deleteUser(Integer id) throws DataAccessException {
-        return 0;
+        Assert.notNull(id);
+        LOGGER.debug("deleteUser",id);
+
+        return userDao.deleteUser(id);
     }
 }
--- service/src/test/java/com/epam/test/UserServiceImplTest.java
+++ service/src/test/java/com/epam/test/UserServiceImplTest.java
@@ -1,11 +1,16 @@
 package com.epam.test;
 
+import com.epam.test.model.User;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.aop.scope.ScopedProxyFactoryBean;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -18,44 +23,70 @@ import static org.junit.Assert.*;
 public class UserServiceImplTest {
     @Autowired
     UserService userService;
-    
-    @org.junit.Before
-    public void setUp() throws Exception {
 
-    }
+    private static final String USER_LOGIN_1="userLogin1";
 
-    @org.junit.After
-    public void tearDown() throws Exception {
 
-    }
 
-    @org.junit.Test
+    @Test
     public void getAllUsers() throws Exception {
+        List<User> users=userService.getAllUsers();
+        assertEquals("",2,users.size());
 
     }
 
-    @org.junit.Test
+    @Test
     public void getUserById() throws Exception {
+        User user=userService.getUserById(1);
+        Assert.assertNotNull(user);
+        Assert.assertNotNull(user.getLogin());
+        Assert.assertEquals((Integer) 1,user.getUserId());
 
     }
 
-    @org.junit.Test
+    @Test
     public void getUserByLogin() throws Exception {
+        User user=userService.getUserByLogin(USER_LOGIN_1);
+        Assert.assertNotNull(user);
+        Assert.assertNotNull(user.getLogin());
+        Assert.assertEquals(USER_LOGIN_1,user.getLogin());
+
 
     }
 
-    @org.junit.Test
+    @Test
     public void addUser() throws Exception {
-
+        User user=new User("us3","pass3");
+        Assert.assertNull(user.getUserId());
+        Integer id=userService.addUser(user);
+        Assert.assertNotNull(id);
     }
 
-    @org.junit.Test
+    @Test
     public void updateUser() throws Exception {
+        User user=userService.getUserByLogin(USER_LOGIN_1);
+        user.setLogin("loginNew1");
+        int count=userService.updateUser(user);
+        Assert.assertEquals(1,count);
+        user=userService.getUserByLogin("loginNew1");
+        Assert.assertNotNull(user);
 
     }
 
-    @org.junit.Test
+    @Test(expected = IllegalArgumentException.class)
+    public void addUserNull(){
+        userService.addUser(new User(null,"pass"));
+    }
+
+    @Test
     public void deleteUser() throws Exception {
+        List<User> users=userService.getAllUsers();
+        int oldsize=users.size();
+
+        userService.deleteUser(1);
+        users=userService.getAllUsers();
+        Assert.assertEquals(oldsize-1,users.size());
+
 
     }
 

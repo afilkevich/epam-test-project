0a447ac476c41db2753033111c606ae1410914f9
afilkevich <afilkevich@ya.ru>
2017-03-09 11:15:18 +0300

    create dao

---
+++ dao/src/main/java/com/epam/project/daoimpl/DepoAndWagonDaoImpl.java
@@ -0,0 +1,85 @@
+package com.epam.project.daoimpl;
+
+import com.epam.project.dao.DepoAndWagonDao;
+import com.epam.project.model.Depo;
+import com.epam.project.model.Wagon;
+import org.springframework.dao.DataAccessException;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Created by master on 9.3.17.
+ */
+public class DepoAndWagonDaoImpl implements DepoAndWagonDao {
+
+    @Override
+    public List<Depo> getAllDepo() throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Depo getDepoById(Integer id) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Depo getDepobyName(String name) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Integer addDepo(Depo depo) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public int updateDepo(Depo depo) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public int deleteDepo(Integer id) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public List<Wagon> getAllWagonByDepo(int depoId) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Wagon getWagonById(Integer id) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Integer addWagon(Wagon wagon) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public int updateWagon(Wagon wagon) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public int deleteWagon(Integer idWagon) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public List<Wagon> getAllWagonByDate(LocalDate from, LocalDate to) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public int getCountOfWagonByDepo(int depoId) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public int getSumofSeatByDepo(int depoId) throws DataAccessException {
+        return 0;
+    }
+}

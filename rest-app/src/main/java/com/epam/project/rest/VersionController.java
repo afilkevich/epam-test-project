3c37d4f437a31e2d04909738b6c30fb2be11cdba
afilkevich <afilkevich@ya.ru>
2017-03-16 12:22:02 +0300

    create rest with versioncontroller

---
+++ rest-app/src/main/java/com/epam/project/rest/VersionController.java
@@ -0,0 +1,19 @@
+package com.epam.project.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by master on 16.3.17.
+ */
+@RestController
+public class VersionController {
+
+    private static final String VERSION="version-1.0";
+
+    @RequestMapping(value ="/version",method = RequestMethod.GET)
+    public String getVersion(){
+        return VERSION;
+    }
+}

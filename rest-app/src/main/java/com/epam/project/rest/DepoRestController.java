8ff5312a615dc6c6bf7576be24706904f5507cde
afilkevich <afilkevich@ya.ru>
2017-03-16 16:05:00 +0300

    add some method in depoController

--- rest-app/src/main/java/com/epam/project/rest/DepoRestController.java
+++ rest-app/src/main/java/com/epam/project/rest/DepoRestController.java
@@ -49,15 +49,30 @@ public class DepoRestController {
 
 // curl -H "Content-Type: application/json" -X POST -d '{"name":"Vitebsk"}' -v localhost:8088/depo/add
     @ResponseBody
+    @ResponseStatus(value = HttpStatus.CREATED)
     @RequestMapping(value="/add",method = RequestMethod.POST)
     public Integer addDepo(@RequestBody Depo depo){
         LOGGER.debug("rest:addDepo",depo);
         Integer id= depoService.addDepo(depo);
         return id;
     }
-    
+
+    // curl -H "Content-Type: application/json" -X PUT -d '{"depoId":"2","name":"Mogilev"}' -v localhost:8088/depo/update
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.ACCEPTED)
+    @RequestMapping(value = "/update", method = RequestMethod.PUT)
+    public Integer updateDepo(@RequestBody Depo depo){
+        LOGGER.debug("rest:updateDepo",depo);
+        return depoService.updateDepo(depo);
+    }
 
 
+  //curl -X DELETE -v localhost:8088/depo/delete/2
+@RequestMapping(value = "/delete/{idDepo}",method = RequestMethod.DELETE)
+    public Integer deleteDepo(@PathVariable(value = "idDepo") Integer idDepo){
+    LOGGER.debug("rest:deleteDepo",idDepo);
+    return depoService.deleteDepo(idDepo);
+}
 
 
 
